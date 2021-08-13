078a2ddbb222238ead69b87a1147be6f2b7198b5
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-08-13 21:55:03 +0700

    Delete homeWork-3-4 directory

--- homeWork-3-4/src/day1/ClassComponent.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-class ClassComponent extends Component {
-    render (){
-        return (
-            <h1> đây là component</h1>
-        )
-    }
-}
-export default ClassComponent;
--- homeWork-3-4/src/day1/FunctionComponent.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function FunctionComponent(){
-    return (
-        <div>
-            <h2>đây là function</h2>
-        </div>
-    );
-}
