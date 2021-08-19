c254f96e0049adeb789c4671c7a20cb34e7132c2
nguyen-anh-duy <85326086+nguyen-anh-duy@users.noreply.github.com>
2021-08-19 21:13:48 +0700

    Add files via upload

---
+++ CI_DBP_D10_NguyenAnhDuy_MidTerm/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import DemoForm from './DemoForm/DemoForm';
+import QuizApp from './QuizApp/QuizApp';
+import SmartPhoneStore from './smartphoneStore/SmartPhoneStore';
+import StudentManagement from './StudentManagement/StudentManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizApp />
+      {/* <StudentManagement /> */}
+      {/* <DemoForm /> */}
+      {/* <SmartPhoneStore /> */}
+    </div>
+  );
+}
+
+export default App;
